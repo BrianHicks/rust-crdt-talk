871f74c940fa67c6ab0dbe0cd7ae102b53a9df07
Brian Hicks <brian@brianthicks.com>
2025-03-06 17:59:32 -0600

    add a sync_both command

--- demo.sh
+++ demo.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 alias phone="$(pwd)/target/release/rust-crdt-talk --store-path phone.json"
 alias computer="$(pwd)/target/release/rust-crdt-talk --store-path computer.json"
+
+sync_both() {
+  phone merge computer.json
+  computer merge phone.json
+}
